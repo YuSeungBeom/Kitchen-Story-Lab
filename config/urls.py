e1fb1da2c9e888a9a708a6ac1b9d09eb8f51d1cf
YuSeungBeom <ykseok518@naver.com>
2025-02-07 01:15:51 +0900

    🔗 Update: Modify URL routing and namespace configurations

--- config/urls.py
+++ config/urls.py
@@ -1,14 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.views.generic.base import RedirectView
-
 
 urlpatterns = [
-    path('', RedirectView.as_view(url='/posts/', permanent=True)),
     path('admin/', admin.site.urls),
-    path('accounts/', include('apps.accounts.urls')),
-    path('posts/', include('apps.posts.urls', 'namespace=posts')),
-    path('recipes/', include('apps.recipes.urls')),
-    path('tips/', include('apps.tips.urls')),
-    path('interactions/', include('apps.interactions.urls')),
-] 
+    path('', include('blog.urls')),
+]
