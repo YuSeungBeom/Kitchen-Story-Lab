591a733e24d302b54a1dba408542c651e6eb245a
YuSeungBeom <ykseok518@naver.com>
2025-02-11 00:03:34 +0900

    🛣️ Update URL patterns

--- blog/urls.py
+++ blog/urls.py
@@ -5,13 +5,12 @@ app_name = 'blog'
 
 urlpatterns = [
     path('', views.PostListView.as_view(), name='post_list'),
-    path('posts/', views.PostListView.as_view(), name='post_list'),
+    path('write/', views.PostCreateView.as_view(), name='post_write'),
     path('post/<int:post_pk>/like/', views.toggle_like, name='toggle_like'),
     path('<int:pk>/', views.PostDetailView.as_view(), name='post_detail'),
-    path('write/', views.PostCreateView.as_view(), name='post_write'),
     path('profile/', views.ProfileView.as_view(), name='profile'),
-    path('<int:pk>/edit/', views.PostUpdateView.as_view(), name='post_update'),
-    path('<int:pk>/delete/', views.PostDeleteView.as_view(), name='post_delete'),
+    path('edit/<int:pk>/', views.PostUpdateView.as_view(), name='post_update'),
+    path('delete/<int:pk>/', views.PostDeleteView.as_view(), name='post_delete'),
     path('<int:post_pk>/comment/create/', views.CommentCreateView.as_view(), name='comment_create'),
     path('comment/<int:pk>/update/', views.CommentUpdateView.as_view(), name='comment_update'),
     path('comment/<int:pk>/delete/', views.CommentDeleteView.as_view(), name='comment_delete'),
