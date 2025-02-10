e3aa4f5f331bfc91fa64493315bf0a69f4b55b15
YuSeungBeom <ykseok518@naver.com>
2025-02-10 23:49:13 +0900

    👔 Set up blog model admin configurations

--- blog/admin.py
+++ blog/admin.py
@@ -24,10 +24,6 @@ class CategoryAdmin(admin.ModelAdmin):
     list_display = ['name', 'order', 'created_at']
     search_fields = ['name']
 
-@admin.register(Tag)
-class TagAdmin(admin.ModelAdmin):
-    list_display = ['name', 'created_at']
-    search_fields = ['name']
 
 @admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
@@ -38,16 +34,9 @@ class CommentAdmin(admin.ModelAdmin):
 class PostLikeAdmin(admin.ModelAdmin):
     list_display = ['post', 'user', 'created_at']
 
-@admin.register(Scrap)
-class ScrapAdmin(admin.ModelAdmin):
-    list_display = ['post', 'user', 'created_at']
 
 @admin.register(Recipe)
 class RecipeAdmin(admin.ModelAdmin):
     list_display = ['post', 'cooking_time', 'difficulty', 'created_at']
     list_filter = ['difficulty']
 
-@admin.register(Tip)
-class TipAdmin(admin.ModelAdmin):
-    list_display = ['post', 'tip_category', 'created_at']
-    search_fields = ['key_points']
