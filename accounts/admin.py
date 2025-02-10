a281e08db3594c155240a75a9271ae18b754df8c
YuSeungBeom <ykseok518@naver.com>
2025-02-10 23:48:30 +0900

    👔 Configure Django admin for user accounts

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
 
-# Register your models here.
+class CustomUserAdmin(UserAdmin):
+    model = User
+    list_display = ['username', 'nickname', 'email', 'is_active']
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': ('nickname', 'profile_image', 'bio')}),
+    )
+
+admin.site.register(User, CustomUserAdmin)
